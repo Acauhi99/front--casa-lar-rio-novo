616a9d29129b001aa0b96aa3109f902fad5fd24d
FernandaRooke <fernanda.r.silva@viannasempre.com.br>
2024-10-21 07:54:55 -0300

    feat: adicionar header com logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,31 @@
-export const Header = () => {
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header-left">
+        {/* vou colocar a logo aqui, em breve*/}
+        <h1>Logo</h1>
+      </div>
+      <nav className="header-right">
+        <ul>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+          <li>
+            <Link to="/donations">Doações</Link>
+          </li>
+          <li>
+            <Link to="/faq">FAQ</Link>
+          </li>
+          <li>
+            <Link to="/volunteers">Voluntários</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
