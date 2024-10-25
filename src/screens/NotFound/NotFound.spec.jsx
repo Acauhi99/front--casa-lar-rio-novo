5fab66388b5ca980dbb64981e434b6a1c35ea8c7
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-25 08:13:30 -0300

    add: pagina de notfound

--- src/screens/NotFound/NotFound.spec.jsx
+++ src/screens/NotFound/NotFound.spec.jsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { NotFound } from './NotFound';
@@ -8,8 +9,20 @@ describe('NotFound Component', () => {
     expect(container).toBeDefined();
   });
 
-  it('should display the correct text', () => {
-    const { getAllByText } = render(<NotFound />);
-    expect(getAllByText('NotFound').length).toBeGreaterThan(0);
+  it('should display the correct header text', () => {
+    const { getByText } = render(<NotFound />);
+    expect(getByText('Página Não Encontrada')).toBeInTheDocument();
+  });
+
+  it('should display the correct message text', () => {
+    const { getByText } = render(<NotFound />);
+    expect(getByText('Infelizmente, o URL que você tentou acessar não é válido.')).toBeInTheDocument();
+  });
+
+  it('should have a button that redirects to home', () => {
+    const { getByText } = render(<NotFound />);
+    const button = getByText('Voltar para Home');
+    expect(button).toBeInTheDocument();
+    expect(button.tagName).toBe('BUTTON');
   });
 });
