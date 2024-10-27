b80e76f70e73f8853aa38ed30aa4a9900e9bb102
Arthur <arthurmedeirosmm010@gmail.com>
2024-10-27 17:39:32 -0300

    add: componente footer com as informações do asilo

--- src/components/Footer/Footer.spec.jsx
+++ src/components/Footer/Footer.spec.jsx
@@ -1,15 +1,19 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-import { Footer } from './Footer';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
 
 describe('Footer Component', () => {
-  it('should render without crashing', () => {
-    const { container } = render(<Footer />);
-    expect(container).toBeDefined();
-  });
+  test('renders footer with correct content', () => {
+    render(<Footer />);
 
-  it('should display the correct text', () => {
-    const { getAllByText } = render(<Footer />);
-    expect(getAllByText('Footer').length).toBeGreaterThan(0);
+    const titleElement = screen.getByText(/Casa Lar Rio Novo/i);
+    expect(titleElement).toBeInTheDocument();
+
+    const addressElement = screen.getByText(/Rua Jacob da Paixao, 115 Rio Novo - MG, 36.150-000/i);
+    expect(addressElement).toBeInTheDocument();
+
+    const instagramLink = screen.getByRole('link', { name: /Instagram/i });
+    expect(instagramLink).toBeInTheDocument();
+    expect(instagramLink).toHaveAttribute('href', 'https://www.instagram.com/casalarderionovo/');
   });
 });
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import Footer from '../../components/Footer/Footer';
 
 export const Home = () => {
   return (
@@ -6,6 +7,10 @@ export const Home = () => {
       <h1 className="home-header">Bem-vindo à Home Page</h1>
       <p className="home-intro">Esta é a página inicial do nosso site. Esperamos que você goste!</p>
       <button className="home-button">Clique Aqui</button>
+
+      <div>
+      <Footer />
+      </div>
     </div>
   );
 }
