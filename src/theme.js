b6a987c540dde9d9d4a6ba3ad5697ec810a785a3
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-19 22:45:22 -0300

    add: padronizado tamanho das fontes

--- src/theme.js
+++ src/theme.js
@@ -13,7 +13,39 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif', // Defina a fonte padrão
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontSize: "3.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "3rem",
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: "2rem",
+      fontWeight: 700,
+    },
+    h5: {
+      fontSize: "1.75rem",
+      fontWeight: 700,
+    },
+    h6: {
+      fontSize: "1.5rem",
+      fontWeight: 700,
+    },
+    body1: {
+      fontSize: "1.125rem",
+      lineHeight: 1.6,
+    },
+    body2: {
+      fontSize: "1rem",
+      lineHeight: 1.5,
+    },
   },
 });
 
