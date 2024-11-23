2716102fe925d55c2a72c2d6e7a493e1994ad43f
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-23 13:19:18 -0300

    refactor: ajuste nos testes

--- src/screens/Home/Home.spec.jsx
+++ src/screens/Home/Home.spec.jsx
@@ -1,51 +1,55 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
 import { Home } from "./Home";
+import theme from "../../theme";
+import "@testing-library/jest-dom";
 
 describe("Home Component", () => {
-  test("renders the main heading", () => {
-    render(<Home />);
-    const mainHeading = screen.getByText(/Ajudamos a transformar/i);
+  it("renders the Banner component", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+    const bannerTitle = screen.getByText(/Transforme Vidas/i);
+    expect(bannerTitle).toBeInTheDocument();
+  });
+
+  it("displays the main heading", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+    const mainHeading = screen.getByRole("heading", {
+      name: /Ajudamos a transformar\.\.\./i,
+      level: 5,
+    });
     expect(mainHeading).toBeInTheDocument();
   });
 
-  test("renders three cards with correct titles and icons", () => {
-    render(<Home />);
-
-    // Obter todos os elementos de heading de nível 6
-    const cardTitles = screen.getAllByRole("heading", { level: 6 });
-
-    // Definir os títulos esperados
-    const expectedTitles = [
+  it("displays each card with correct title and description", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+    const cardTitles = [
       "Atendimento Humanizado",
       "Infraestrutura Dedicada",
       "Programas de Inclusão",
     ];
 
-    // Verificar se cada título esperado está presente
-    expectedTitles.forEach((title) => {
-      const matchingTitle = cardTitles.find(
-        (cardTitle) => cardTitle.textContent === title
-      );
-      expect(matchingTitle).toBeInTheDocument();
-    });
-
-    // Verificar se há exatamente três títulos de cards
-    expect(cardTitles.length).toBeGreaterThanOrEqual(3); // Ajuste conforme necessário
-  });
-
-  test("renders card texts correctly", () => {
-    render(<Home />);
-
-    // Verifique trechos do texto de cada card
-    const cardTexts = [
-      /oferecer um atendimento humanizado/i,
-      /oferecer um ambiente seguro e acolhedor/i,
-      /envolve laços sociais e interação com a comunidade/i,
-    ];
-
-    cardTexts.forEach((text) => {
-      expect(screen.getByText(text)).toBeInTheDocument();
+    cardTitles.forEach((title) => {
+      const cardTitle = screen.getByRole("heading", { name: title, level: 6 });
+      expect(cardTitle).toBeInTheDocument();
     });
   });
 });
